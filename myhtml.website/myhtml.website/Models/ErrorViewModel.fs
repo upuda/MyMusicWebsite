5d37574a6305a64f5c1706a47005e4f9ff822972
upuda <Upanya.Udayendran@my-aolcc.com>
2025-01-05 23:55:01 -0500

    commit after completing my music website

---
+++ myhtml.website/myhtml.website/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace myhtml.website.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
